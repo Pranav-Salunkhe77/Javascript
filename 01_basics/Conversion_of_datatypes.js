d08af1cfd194218b4bb269ffd3dfa03a57b68372
Pranav-Salunkhe77 <pranavsalunkhe777@gmail.com>
2026-05-31 14:30:43 +0000

    Updated with logic

--- 01_basics/Conversion_of_datatypes.js
+++ 01_basics/Conversion_of_datatypes.js
@@ -45,22 +45,34 @@
 // let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);//If any one of number is present in string then the concat operation
+//  is performed otherwise the addition operation is performed 
 
-// console.log( (3 + 4) * 5 % 3);
+// console.log(1 + 2);//The values are in normal not in string then the addition operation is performed.
+
+// console.log("1" + 2 + 2);// The operation is performed from left to right so first 1+2 is performed and the result is "12" then "12"+2 is performed and the result is "122" 
+//BUt when coming to left to ringt first double quoted string is present then the concat operation is performed.
+
+
+// console.log(1 + 2 + "2");// The operation is performed from left to right so first 1+2 is performed and the result is 3 then 3+"2" is performed and the result is "32"  
+// But when coming to left to ringt only single number is present then the addition operation is performed.
+
+// console.log( (3 + 4) *( 5 % 3));//Right methode to write code is to use paranthesis to avoid confusion and to get the correct result.    
 
 // console.log(+true);
-// console.log(+"");
+// console.log(+"hello");//The logic behind this is that when we try to convert a non-numeric string to a number, 
+// it results in NaN (Not a Number). The unary plus operator attempts to convert 
+// the operand to a number, and since "hello" cannot be converted to a valid number, it returns NaN.  
+// console.log(+"33");
+// console.log("hello" + "");//in simple the hello is apped with empty string and the result is "hello" because when we add an empty string to any string.
+
 
 // let num1, num2, num3
 
 // num1 = num2 = num3 = 2 + 2
 // console.log(num1, num2, num3);
-// let gameCounter = 100
-// ++gameCounter;
+
+// let gameCounter = 100+3;
 // console.log(gameCounter);
 
 // // link to study
